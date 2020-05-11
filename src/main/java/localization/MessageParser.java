8970f5b1d5ba0bf9cef165d5767a13e962411153
Victor Moraes <dt.victor@gmail.com>
2020-05-11 20:55:39 -0300

    Pegando idioma pelo log4j2.properties

--- src/main/java/localization/MessageParser.java
+++ src/main/java/localization/MessageParser.java
@@ -1,12 +1,16 @@
 package localization;
 
+import config.ConfigurationLog4j;
+import org.aeonbits.owner.ConfigCache;
+
 import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class MessageParser {
 
-    private ResourceBundle bundle = ResourceBundle.getBundle("messages/messages", Locale.forLanguageTag("en-US"));
+    private ResourceBundle bundle= ResourceBundle.getBundle("messages/messages",
+            Locale.forLanguageTag(ConfigCache.getOrCreate(ConfigurationLog4j.class).language()));
 
     public String parse(String label, Object[] params) {
         MessageFormat formatter = new MessageFormat(bundle.getString(label));
