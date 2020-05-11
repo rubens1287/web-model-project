d3b6c063c34e75871dfe61a516181842e245f9b9
Victor Moraes <dt.victor@gmail.com>
2020-05-11 19:23:58 -0300

    Nova classe de parser das mensagens de log

---
+++ src/main/java/localization/MessageParser.java
@@ -0,0 +1,19 @@
+package localization;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MessageParser {
+
+    private ResourceBundle bundle = ResourceBundle.getBundle("messages/messages", Locale.forLanguageTag("en-US"));
+
+    public String parse(String label, Object[] params) {
+        MessageFormat formatter = new MessageFormat(bundle.getString(label));
+        return formatter.format(params);
+    }
+
+    public String parse(String label){
+        return bundle.getString(label);
+    }
+}
