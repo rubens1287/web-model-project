5602671d2523bda29c2405244d04c883935d44a8
Victor Moraes <dt.victor@gmail.com>
2020-05-11 21:07:45 -0300

    Usando ResourceBundle para ler arquivo properties

--- src/main/java/localization/MessageParser.java
+++ src/main/java/localization/MessageParser.java
@@ -1,23 +1,21 @@
 package localization;
 
-import config.ConfigurationLog4j;
-import org.aeonbits.owner.ConfigCache;
-
 import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class MessageParser {
 
-    private ResourceBundle bundle= ResourceBundle.getBundle("messages/messages",
-            Locale.forLanguageTag(ConfigCache.getOrCreate(ConfigurationLog4j.class).language()));
+    private static ResourceBundle prop = ResourceBundle.getBundle("log4j2");
+    private static ResourceBundle bundle = ResourceBundle.getBundle("messages/messages",
+            Locale.forLanguageTag(prop.getString("language")));
 
     public String parse(String label, Object[] params) {
         MessageFormat formatter = new MessageFormat(bundle.getString(label));
         return formatter.format(params);
     }
 
-    public String parse(String label){
+    public String parse(String label) {
         return bundle.getString(label);
     }
 }
